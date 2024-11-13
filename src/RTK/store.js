ef89690f6243600b42cac68e9becb0e04d128f45
anurag <anchouhan@bestpeers.com>
2024-11-13 10:32:36 +0530

    implement redux-persist for redux store

--- src/RTK/store.js
+++ src/RTK/store.js
@@ -1,13 +1,26 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-// import {persistReducers, persistStore } from 'redux-persist'
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import productData from "./productSlice";
 import userData from "./userSlice";
+
+const persistConfig = {
+  key: "redux-persist-config",
+  storage,
+};
 
 const rootreducer = combineReducers({
   product: productData,
   user: userData,
 });
 
-export const store = configureStore({
-  reducer: rootreducer,
+const persistedReducer = persistReducer(persistConfig, rootreducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
 });
+
+const persistor = persistStore(store);
+
+// export default store;
+export { store,persistor };
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import { store } from './RTK/store.js'
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './RTK/store.js'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
+    <PersistGate persistor={persistor}>
     <App />
+    </PersistGate>
   </Provider>,
 )
